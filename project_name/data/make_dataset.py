feb8da19b00ebbd48c0214a04b5af4f723650042
Jan Wasilewski <janwasilewski144@op.pl>
2022-01-15 20:28:13 +0100

    main

--- project_name/data/make_dataset.py
+++ project_name/data/make_dataset.py
@@ -140,8 +140,8 @@ def write_as_CSV(target_file, line_data, category_dict):
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+#@click.argument('output_filepath', type=click.Path())
+def main(input_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
